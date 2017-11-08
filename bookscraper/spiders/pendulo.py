46b0d92c05350d4a6da578a74b3477046edbe93d
anakaren <vedaanam@gmail.com>
2017-11-08 13:45:12 -0800

    pendulo spider added

--- bookscraper/spiders/pendulo.py
+++ bookscraper/spiders/pendulo.py
@@ -11,12 +11,9 @@ class ElPenduloSpider(scrapy.Spider):
         isbn_list = binary_string.decode("utf-8").split("\n")
 
         self.details_headers = {
-            "Host": "www.pendulo.com",
+            "Host": "pendulo.com",
             "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:56.0) Gecko/20100101 Firefox/56.0",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-            "Accept-Language": "en-US,en;q=0.5",
             "Accept-Encoding": "gzip, deflate, br",
-            "Referer": "https://pendulo.com/"
         }
 
         for isbn in isbn_list:
@@ -32,7 +29,7 @@ class ElPenduloSpider(scrapy.Spider):
             "url": response.url,
             "title": self.clean_text(response.selector.xpath('//div/h1[@itemprop="name"]/text()').extract_first()),
             "content": self.clean_text(response.selector.xpath('//div[@id="productoPR_descripcion"]/text()').extract_first()),
-            "author": self.clean_text(response.selector.xpath('//div/h2[@id="productoPR_autor"]/text()').extract_first()),
+            "author": self.clean_text(response.selector.xpath('//div/h2[@id="productoPR_autor"]/a/text()').extract_first()),
             "editorial": self.clean_text(response.selector.xpath('//dl/dd/a[@itemprop="publisher"]/text()').extract_first()),
             "price": self.clean_price(response.selector.xpath('//div/p/span[@itemprop="price"]/text()').extract_first()),
             "ISBN": self.clean_price(response.selector.xpath('//dd[@itemprop="ISBN"]/text()').extract_first())
