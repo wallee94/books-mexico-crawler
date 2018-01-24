157ab081ad50c6d0c8290650d6112c46d45af0cf
Walther Lee <walthere.lee@gmail.com>
2018-01-24 14:50:15 -0800

    urls scraper added for laleo.com

--- bookscraper/spiders/laleo.py
+++ bookscraper/spiders/laleo.py
@@ -1,41 +1,67 @@
 import scrapy
+import json
 
 
 class LaleoSpider(scrapy.Spider):
     name = "laleo.com"
 
     def start_requests(self):
-        urls = [
-            'https://www.laleo.com/libros-c-127.html'
-        ]
+        # get category codes
+        url = "https://www.laleo.com/libros-c-127.html"
+        yield scrapy.Request(url=url, callback=self.parse_category_ids)
 
-        self.listing_headers={
-            "Host": "www.laleo.com",
-            "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-            "Accept-Language": "en-US,en;q=0.5",
-            "Connection": "keep-alive"
-        }
+    def parse_category_ids(self, response):
+        for category_id in response.selector.re(r"127_(\d+)"):
+            body = {
+                "container": "listing",
+                "page": "0",
+                "limit": "20",
+                "type": "category",
+                "group": category_id,
+                "filter": "undefined",
+                "sort": "undefined",
+                "keywords": "",
+            }
+            url = "https://www.laleo.com/load_more_products.php"
+            meta = {
+                "page": 0,
+                "category_id": category_id,
+            }
+            yield scrapy.FormRequest(url=url, formdata=body, callback=self.parse_category_books, meta=meta)
 
-        for url in urls:
-            page = url.split("127")[1]
-            new_url = url.split("127")[0] + "_" + "36" + str(int(page) + 1)
-            yield scrapy.Request(url=url, callback=self.parse, headers=self.listing_headers)
+    def parse_category_books(self, response):
+        response_obj = json.loads(response.body_as_unicode())
+        rq_status = response_obj.get("rq_status")
+        if not rq_status or rq_status == "empty":
+            return
 
-    def parse(self, response):
-        page = response.url.split("127")[-1]
+        products = response_obj.get("products")
+        if not products:
+            return
 
-        for a_tags in response.selector.xpath('//a[@class="product_img_link"]/@href'):
-            url = a_tags.extract()
-            data = {
-                "url": url
+        for product in products.get("listing", []):
+            yield {
+                "url": product.get("link")
             }
 
-            yield data
-
-        meta = {
-            "dont_redirect": True
+        try:
+            actual_page = int(response.meta.get("page"))
+        except ValueError:
+            return
+        category_id = response.meta.get("category_id")
+        body = {
+            "container": "listing",
+            "page": str(actual_page + 1),
+            "limit": "20",
+            "type": "category",
+            "group": category_id,
+            "filter": "undefined",
+            "sort": "undefined",
+            "keywords": "",
         }
-        new_url = response.url.split("&p=")[0] + "&p=" + str(int(page) + 1)
-        yield scrapy.Request(url=new_url, callback=self.parse,
-                             headers=self.listing_headers, meta=meta)
+        meta = {
+            "page": body["page"],
+            "category_id": category_id,
+        }
+        url = "https://www.laleo.com/load_more_products.php"
+        yield scrapy.FormRequest(url=url, formdata=body, callback=self.parse_category_books, meta=meta)
