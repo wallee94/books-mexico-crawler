c9e20081e3d455f4663ba1b9112bff9d92323bae
an_sepulveda <vedaanam@gmail.com>
2018-01-29 16:07:37 -0800

    libreria morelos spider added

--- bookscraper/spiders/libreriamorelos.py
+++ bookscraper/spiders/libreriamorelos.py
@@ -2,16 +2,16 @@ import scrapy
 
 import re
 
-class LibreriaLeonSpider(scrapy.Spider):
-    name = "librerialeon.com.mx"
+class LibreriaMorelosSpider(scrapy.Spider):
+    name = "libreriamorelos.mx"
 
     def start_requests(self):
         urls = [
-            'http://www.librerialeon.com.mx/sitemaplibros.xml.php?pag=' + str(i) for i in range(1,18)
+            'https://libreriamorelos.mx/' + str(i) + '/titulo' for i in range(29890,88200)
         ]
 
         self.details_headers = {
-            "Host": "www.librerialeon.com.mx",
+            "Host": "libreriamorelos.mx",
             "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0",
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
             "Accept-Language": "en-US,en;q=0.5",
@@ -22,12 +22,9 @@ class LibreriaLeonSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse, headers=self.details_headers)
 
     def parse(self, response):
-        response.selector.register_namespace('d', 'http://www.google.com/schemas/sitemap/0.84')
-        locs = response.selector.xpath("//d:loc/text()")
-
-        for loc in locs:
-            url = loc.extract()
-            if "libro" in url:
+        if response.selector.xpath('//h1[@class="product-title"]'):
+            url = response.url.strip()
+            if url:
                 url = url.replace("http:", "https:")
                 data = {
                     "url": re.sub("^h?t?t?p?s?:?/?/?w?w?w?\.?", "", url)
